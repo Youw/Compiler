63e4f9cbf93b924678455ad9c593d8b284b84aac
Ihor Dutchak <ihor.youw@gmail.com>
2015-04-06 02:58:35 +0300

    lexem and error lexem

--- src/lexical/lexem.cpp
+++ src/lexical/lexem.cpp
@@ -1,12 +1,21 @@
 #include "lexem.h"
 
-Lexem::Lexem()
-{
-
-}
-
 Lexem::~Lexem()
 {
 
 }
 
+ErrorLexem::ErrorLexem(const string& error_info): info(error_info)
+{
+
+}
+
+LexemType ErrorLexem::type() const
+{
+  return LexemType::ERROR;
+}
+
+const string& ErrorLexem::name() const
+{
+  return info;
+}
--- src/lexical/lexem.h
+++ src/lexical/lexem.h
@@ -1,12 +1,26 @@
 #ifndef LEXEM_H
 #define LEXEM_H
 
+#include <config.h>
+#include "lexeminfo.h"
 
 class Lexem
 {
 public:
-  Lexem();
-  ~Lexem();
+  virtual LexemType type() const = 0;
+  virtual const string &name() const = 0;
+
+  virtual ~Lexem();
+};
+
+class ErrorLexem: Lexem
+{
+  string info;
+public:
+  ErrorLexem(const string& error_info);
+
+  LexemType type() const override;
+  const string &name() const override;
 };
 
 #endif // LEXEM_H
